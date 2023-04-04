751369b119442bf606c69f5ef633ab2c720b5173
AnkhSquirrel <suchkp@tutanota.com>
2023-04-04 22:06:17 +0200

    Button generation

--- scripts.js
+++ scripts.js
@@ -1,23 +1,24 @@
-const duckduckgoQuery = "https://duckduckgo.com/?t=ffab&q="
-const youtubeQuery = "https://www.youtube.com/results?search_query="
-
 document.addEventListener("DOMContentLoaded", () => {
 
-    window.browser = window.browser || window.chrome;
-
     const textField = document.getElementById('text-field');
-    const duckduckgoBtn = document.getElementById('duckduckgo-btn');
-    const youtubeBtn = document.getElementById('youtube-btn');
 
-    duckduckgoBtn.addEventListener('click', () => {
-        executeQuery(duckduckgoQuery, textField.value)
-    });
-
-    youtubeBtn.addEventListener('click', () => {
-        executeQuery(youtubeQuery, textField.value)
-    });
+    fetch('queries.json')
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(query => {
+            const myDiv = document.getElementById("buttons");
+            const myButton = document.createElement("button");
+            myButton.textContent = query.name;
+            myButton.addEventListener('click', () => {
+                executeQuery(query.url, textField.value)
+            });
+            myDiv.appendChild(myButton);
+        })
+    })
+    .catch(error => console.error(error));
 
 });
+
 
 function executeQuery(website, query) {
     window.open(website + query, "_blank");
